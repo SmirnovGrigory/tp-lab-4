1ed7c08a2195cbff2a28fdf0560da4cbe5b9a746
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2020-12-03 11:32:08 +0300

    more tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,15 +1,59 @@
 #include "gtest/gtest.h"
 #include "automata.h"
 
-TEST(test_1, test1)
+TEST(test_off, test1)
 {
-    Automata test;
-    EXPECT_EQ(OFF, test.getState());
+    Automata A;
+    EXPECT_EQ(OFF, A.getState());
 }
 
-TEST(on, test2)
+TEST(test_on, test2)
 {
-    Automata test;
-    test.on();
-    EXPECT_EQ(WAIT, test.getState());
+    Automata A;
+    A.on();
+    EXPECT_EQ(WAIT, A.getState());
 }
+
+TEST(test_coin, test3)
+{
+    Automata A;
+    A.on();
+    A.coin(5);
+    EXPECT_EQ(ACCEPT, A.getState());
+}
+
+TEST(test_cancel, test4)
+{
+    Automata A;
+    A.on();
+    A.coin(3);
+    EXPECT_EQ(3, A.cancel());
+}
+
+TEST(test_cancel2, test5)
+{
+    Automata A;
+    A.on();
+    EXPECT_EQ(-1, A.cancel());
+}
+
+TEST(test_cancdrink, test6)
+{
+    Automata A;
+    A.on();
+    A.coin(20)
+    EXPECT_EQ(20, A.choice("milk"));
+}
+
+TEST(test_drink, test7)
+{
+    Automata A;
+    A.on();
+    A.coin(13)
+    EXPECT_EQ(3, A.choice("hot water"));
+}
+
+
+
+
+
